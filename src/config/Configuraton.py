b42cdcb3b7446c4a7659dfba8f37400276659497
Axel Arteaga <axelarteaga21@gmail.com>
2026-02-09 14:06:07 -0600

    <add>[CallTaxiComfortPage.py] The method on click call taxi

---
+++ src/config/Configuraton.py
@@ -0,0 +1,1 @@
+URBAN_ROUTER_URL = 'https://cnt-e04364fb-8b44-48d1-a5f3-593780aa4544.containerhub.tripleten-services.com/?lng=es'
--- src/test/TestUrbanRoutes.py
+++ src/test/TestUrbanRoutes.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
-from ..data import  data
-from ..config import configuraton
-from ..model.FromToPageUrbanRoutes import FromToPageUrbanRoutes
+from src.data import  Data
+from src.config import Configuraton
+from src.model.FromToPageUrbanRoutes import FromToPageUrbanRoutes
+from src.model.CallTaxiComfortPage import CallTaxiComfortPage
+
 class TestUrbanRoutes:
 
     driver = None
@@ -13,17 +15,24 @@ class TestUrbanRoutes:
         # capabilities = DesiredCapabilities. CHROME
         # capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
         cls.driver = webdriver.Chrome()
-        cls.driver.get(configuraton.URBAN_ROUTER_URL)
+        cls.driver.get(Configuraton.URBAN_ROUTER_URL)
 
+    # Configurar la dirección
     def test_set_route(self):
         routes_page = FromToPageUrbanRoutes(self.driver)
-        address_from = data.ADDRESS_FROM # 'East 2nd Street, 601'
-        address_to = data.ADDRESS_TO # '1300 1st St'
+        address_from = Data.ADDRESS_FROM # 'East 2nd Street, 601'
+        address_to = Data.ADDRESS_TO # '1300 1st St'
         routes_page.wait_for_load_address_field() # wait 5s
         routes_page.set_route(address_from, address_to)# write address on fields
         assert routes_page.get_from() == address_from
         assert routes_page.get_to() == address_to
 
+    #Seleccionar la tarifa Comfort.
+    def test_on_clic_call_taxi(self):
+        call_taxi = CallTaxiComfortPage(self.driver)
+        # assert call_taxi.get_default_mode_route() == 'Flash'# check the default selected mode route 'Flash'
+        # assert 'car' in call_taxi.get_default_service_fee()# check the default selected service fee 'taxi'
+        call_taxi.on_click_call_taxi_button()
 
     @classmethod
     def teardown_class(cls):
